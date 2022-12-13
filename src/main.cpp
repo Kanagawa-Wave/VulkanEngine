621928d312c2f1eb8136d287067fc74841cb08f1
Yuqi_Lu <lu_yuqi@outlook.com>
2022-12-13 20:19:43 +0800

    Add glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 #define GLFW_INCLUDE_VULKAN
+
 #include <GLFW/glfw3.h>
+
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 int main() {
     glfwInit();
@@ -13,6 +17,10 @@ int main() {
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
     std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
