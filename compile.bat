60b4a5e71eb0e7f79643f5db45c7538a3df1511c
Yuqi_Lu <lu_yuqi@outlook.com>
2022-12-15 20:27:16 +0800

    Edit compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,2 @@
-C:/VulkanSDK/1.3.236.0/Bin/glslc.exe shaders/triangle.vert -o cmake-build-debug/VulkanEngine/triangle.vert.spv
-C:/VulkanSDK/1.3.236.0/Bin/glslc.exe shaders/triangle.frag -o cmake-build-debug/VulkanEngine/triangle.frag.spv
-C:/VulkanSDK/1.3.236.0/Bin/glslc.exe shaders/triangle.vert -o cmake-build-release/VulkanEngine/triangle.vert.spv -O
-C:/VulkanSDK/1.3.236.0/Bin/glslc.exe shaders/triangle.frag -o cmake-build-release/VulkanEngine/triangle.frag.spv -O
+@echo off
+for /r shaders/ %%i in (*.vert, *.frag) do glslc %%i -o %%i.spv
